fcaa8b68cea9067eae1190c366ed4d98f0940275
Karl Haworth <58607256+karlhaworth@users.noreply.github.com>
2024-05-10 11:59:42 -0400

    Update zsh-setup.sh

--- mac/scripts/zsh-setup.sh
+++ mac/scripts/zsh-setup.sh
@@ -16,3 +16,12 @@ sed -ir 's|^plugins=(\(.*\))|plugins=(\1 kubectl history sudo)|g' .zshrc
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 gsed -i 's|ZSH_THEME=.*|ZSH_THEME="powerlevel10k/powerlevel10k"|g' ~/.zshrc
+
+#### Autocomplete
+
+gsed -i '1s|^|source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh\n|' .zshrc
+
+#### OTHERS
+
+gsed -i '1s|^|export PATH=/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH\n|' .zshrc
+gsed -i '1s|^|export PATH=/opt/homebrew/opt/gnu-sed/libexec/gnubin:$PATH\n|' .zshrc
