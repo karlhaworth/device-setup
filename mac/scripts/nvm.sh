7c11b0b84a0c9d948432df6309f90bd9227a23f7
Karl Haworth <58607256+karlhaworth@users.noreply.github.com>
2024-11-13 14:07:02 -0600

    Update nvm.sh

--- mac/scripts/nvm.sh
+++ mac/scripts/nvm.sh
@@ -1,3 +1,3 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
 nvm install --lts --default
