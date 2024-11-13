079e95727b265290f6422c3d39fa6038064252e7
Karl Haworth <58607256+karlhaworth@users.noreply.github.com>
2024-11-13 13:57:53 -0600

    Update start.sh

--- mac/start.sh
+++ mac/start.sh
@@ -6,7 +6,7 @@ xcode-select --install
 
 ### BREW
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew bundle install --file=brew/Brewfile
 case $type in  
