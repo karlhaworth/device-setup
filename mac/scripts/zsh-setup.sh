62e0119c6e3b485a85dd19db4bfa8e547dd12ad8
xx <xx>
2024-11-19 09:51:03 -0500

    update paths

--- mac/scripts/zsh-setup.sh
+++ mac/scripts/zsh-setup.sh
@@ -8,7 +8,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 #### Add OhMyZSH plugins
 
-sed -ir 's|^plugins=(\(.*\))|plugins=(\1 kubectl history sudo)|g' .zshrc
+sed -ir 's|^plugins=(\(.*\))|plugins=(\1 kubectl history sudo)|g' ~/.zshrc
 
 #### Set Theme - PowerLevel10k
 
@@ -19,9 +19,9 @@ gsed -i 's|ZSH_THEME=.*|ZSH_THEME="powerlevel10k/powerlevel10k"|g' ~/.zshrc
 
 #### Autocomplete
 
-gsed -i '1s|^|source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh\n|' .zshrc
+gsed -i '1s|^|source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh\n|' ~/.zshrc
 
 #### OTHERS
 
-gsed -i '1s|^|export PATH=/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH\n|' .zshrc
-gsed -i '1s|^|export PATH=/opt/homebrew/opt/gnu-sed/libexec/gnubin:$PATH\n|' .zshrc
+gsed -i '1s|^|export PATH=/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH\n|' ~/.zshrc
+gsed -i '1s|^|export PATH=/opt/homebrew/opt/gnu-sed/libexec/gnubin:$PATH\n|' ~/.zshrc
