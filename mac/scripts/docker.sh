fee0dcdbb5836a896adea599db0e383b8ce03500
karlhaworth <github@karlhaworth.com>
2025-04-25 12:38:39 -0400

    remove comment

--- mac/scripts/docker.sh
+++ mac/scripts/docker.sh
@@ -3,7 +3,7 @@ brew install docker-buildx
 
 FILE="$HOME/.docker/config.json"
 if [ ! -f "$FILE" ]; then
-  mkdir -p "$(dirname "$FILE")" # Ensure the directory exists
+  mkdir -p "$(dirname "$FILE")"
   echo '{ "cliPluginsExtraDirs": ["/opt/homebrew/lib/docker/cli-plugins"] }' > "$FILE"
 else
   jq '.cliPluginsExtraDirs = ["/opt/homebrew/lib/docker/cli-plugins"]' "$FILE" > temp.json && mv temp.json "$FILE"
