57b914531177bd81c7600b0a31d6111d4d9deaac
karlhaworth <github@karlhaworth.com>
2025-04-25 11:27:12 -0400

    updates

---
+++ mac/scripts/home.sh
@@ -0,0 +1,3 @@
+brew install backblaze
+
+brew install --cask mullvadvpn
--- mac/scripts/krew-plugins.sh
+++
@@ -1,8 +0,0 @@
-kubectl krew install modify-secret
-kubectl krew install neat
-kubectl krew index add kvaps https://github.com/kvaps/krew-index
-kubectl krew install kvaps/node-shell
-kubectl krew upgrade sick-pods
-kubectl krew install sniff
-kubectl krew install konfig
-kubectl krew install get-all
