f4d6c242af5d5d510c246fa866b82f6ca8699662
Karl Haworth <58607256+karlhaworth@users.noreply.github.com>
2025-04-25 08:22:33 -0700

    Create misc.sh

---
+++ mac/scripts/misc.sh
@@ -0,0 +1,31 @@
+# GNU Core Utils
+brew install coreutils
+brew install autoconf
+brew install bash
+brew install binutils
+brew install diffutils
+brew install ed
+brew install findutils
+brew install flex
+brew install gawk
+brew install gnu-indent
+brew install gnu-sed
+brew install gnu-tar
+brew install gnu-which
+brew install grep
+brew install gzip
+brew install less
+brew install make
+brew install nano
+brew install screen
+brew install watch
+brew install wdiff
+brew install wget
+brew install zip
+
+
+brew install terraform
+
+brew install shellcheck
+
+brew install dockutil
