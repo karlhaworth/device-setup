66fac25d0ca0c76d1c539f099f28b209af0b962d
Karl Haworth <58607256+karlhaworth@users.noreply.github.com>
2025-04-25 08:40:31 -0700

    Update start.sh

--- mac/start.sh
+++ mac/start.sh
@@ -12,43 +12,18 @@ echo >> ~/.zprofile
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-brew bundle install --file=brew/Brewfile
-case $type in  
-  w|W) brew bundle install --file=brew/Brewfile.work ;; 
-  h|H) brew bundle install --file=brew/Brewfile.home ;; 
-esac
+### Node
 
-### NVM
+bash scripts/node.sh
 
-bash scripts/nvm.sh
+### Java
 
-### JENV
-
-bash scripts/jenv.sh
+bash scripts/java.sh
 
 ### ZSH
 
 bash scripts/zsh-setup.sh
 
-### DOCK
-
-pip install pyobjc docklib
-
-case $type in  
-  w|W) python scripts/dock.work.py ;; 
-  h|H) python scripts/dock.home.py ;; 
-esac
-
-### KREW Plugins
-
-bash scripts/krew-plugins.sh
-
 ### GIT
 
 bash scripts/git-setup.sh
-
-### Extras
-
-#### Set Go to another directory
-
-go env -w GOPATH=$HOME/.go
