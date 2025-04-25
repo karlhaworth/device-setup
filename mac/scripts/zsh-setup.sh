956c23b60d2470bfe645c87e49771d3ae99b3e16
karlhaworth <github@karlhaworth.com>
2025-04-25 11:28:41 -0400

    update cmd

--- mac/scripts/zsh-setup.sh
+++ mac/scripts/zsh-setup.sh
@@ -4,7 +4,7 @@
 
 #### https://ohmyz.sh
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #### Add OhMyZSH plugins
 
