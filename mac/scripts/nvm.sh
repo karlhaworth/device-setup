c0e39a8a3906a4aaf9e02664c5647d6c71cff43b
Karl Haworth <58607256+karlhaworth@users.noreply.github.com>
2025-04-25 08:09:49 -0700

    Update nvm.sh

--- mac/scripts/nvm.sh
+++ mac/scripts/nvm.sh
@@ -1,11 +1,15 @@
 brew install nvm
 
+mkdir ~/.nvm
+
 cat << 'EOF' | tee -a ~/.zshrc > /dev/null
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
-EOF
+
+
+source ~/.zshrc
 
 nvm install --lts --default
