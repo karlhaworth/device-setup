22d7254b30fb74cabad6d07fc323e3f5a73392bd
Karl Haworth <58607256+karlhaworth@users.noreply.github.com>
2025-04-25 08:06:07 -0700

    Create docker.sh

---
+++ mac/scripts/docker.sh
@@ -0,0 +1,14 @@
+brew install docker
+brew install docker-buildx
+
+FILE="$HOME/.docker/config.json"
+if [ ! -f "$FILE" ]; then
+  mkdir -p "$(dirname "$FILE")" # Ensure the directory exists
+  echo '{ "cliPluginsExtraDirs": ["/opt/homebrew/lib/docker/cli-plugins"] }' > "$FILE"
+else
+  jq '.cliPluginsExtraDirs = ["/opt/homebrew/lib/docker/cli-plugins"]' "$FILE" > temp.json && mv temp.json "$FILE"
+fi
+
+brew install colima
+
+brew services start colima
