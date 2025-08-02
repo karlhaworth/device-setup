0e33af71afc379f27e7de4477557ff284ac70ad5
Karl Haworth <58607256+karlhaworth@users.noreply.github.com>
2025-08-02 15:13:18 -0400

    Update java.sh

--- mac/scripts/java.sh
+++ mac/scripts/java.sh
@@ -10,8 +10,8 @@ sdk default java 17.0.12-tem
 sdk install maven 3.9.10
 sdk default maven 3.9.10
 
-sdk install gradle 3.9.10
-sdk default gradle 3.9.10
+sdk install gradle 9.0.0
+sdk default gradle 9.0.0
 
 brew install intellij-idea
 
