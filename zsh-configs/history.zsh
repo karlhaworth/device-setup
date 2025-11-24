a019d90602b328bca2fb5f2bd3313cc1c3f83c8f
karlhaworth <github@karlhaworth.com>
2025-11-24 12:36:03 -0500

    add history

---
+++ zsh-configs/history.zsh
@@ -0,0 +1,3 @@
+setopt APPEND_HISTORY           # Save history when a shell exits
+setopt SHARE_HISTORY            # Share history across all sessions
+setopt HIST_IGNORE_ALL_DUPS     # Ignore duplicate entries
