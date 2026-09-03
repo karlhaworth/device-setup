fef7498733642e3f9e1b01304d9a401444b652aa
karlhaworth <58607256+karlhaworth@users.noreply.github.com>
2026-09-03 13:25:38 -0400

    lazy load vscode

--- zsh-configs/vscode.zsh
+++ zsh-configs/vscode.zsh
@@ -1,2 +1,11 @@
 export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
-[[ "$TERM_PROGRAM" == "vscode" ]] && . "$(code --locate-shell-integration-path zsh)"
+
+if [[ "$TERM_PROGRAM" == "vscode" ]]; then
+  __vscode_shell_integration="/Applications/Visual Studio Code.app/Contents/Resources/app/out/vs/workbench/contrib/terminal/common/scripts/shellIntegration-rc.zsh"
+  if [[ -r "$__vscode_shell_integration" ]]; then
+    . "$__vscode_shell_integration"
+  else
+    . "$(code --locate-shell-integration-path zsh)"
+  fi
+  unset __vscode_shell_integration
+fi
